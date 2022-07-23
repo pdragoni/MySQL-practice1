8660b0b9a18d77ba09a946df13404058683c7864
paulo_dragoni <paulohbfdf@gmail.com>
2022-07-23 12:36:37 -0300

    Exibe 3 colunas: cancoes, artistas e albuns com valores respectivos 40, 6, 10.

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+SELECT 
+COUNT(Ca.id) AS cancoes,
+COUNT(Ar.id) AS artistas,
+COUNT(Al.id) AS albuns
+FROM SpotifyClone.cancoes Ca
+LEFT JOIN SpotifyClone.artistas Ar
+ON Ca.id = Ar.id
+LEFT JOIN SpotifyClone.albuns Al
+ON Ca.id = Al.id;
