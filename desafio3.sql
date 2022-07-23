980bc19357b4fc63c1e985490039125def21fed2
paulo_dragoni <paulohbfdf@gmail.com>
2022-07-23 13:56:10 -0300

    Estabelece Aliases para tabelas no desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,12 +1,12 @@
 SELECT
-	usuario.nome AS usuario,
-    COUNT(reproducoes_por_usuario.cancoes_id) AS qtde_musicas_ouvidas,
-    ROUND(SUM(cancoes.duracao_segundos) / 60, 2) AS total_minutos
-FROM SpotifyClone.usuario
-LEFT JOIN SpotifyClone.reproducoes_por_usuario
-ON reproducoes_por_usuario.usuario_id = usuario.id
-RIGHT JOIN SpotifyClone.cancoes
-ON cancoes.id = reproducoes_por_usuario.cancoes_id
-WHERE usuario.id IS NOT NULL
-GROUP BY usuario.id
+	u.nome AS usuario,
+  COUNT(rpu.cancoes_id) AS qtde_musicas_ouvidas,
+  ROUND(SUM(c.duracao_segundos) / 60, 2) AS total_minutos
+FROM SpotifyClone.usuario AS u
+LEFT JOIN SpotifyClone.reproducoes_por_usuario AS rpu
+ON rpu.usuario_id = u.id
+RIGHT JOIN SpotifyClone.cancoes AS c
+ON c.id = rpu.cancoes_id
+WHERE u.id IS NOT NULL
+GROUP BY u.id
 ORDER BY usuario ASC;
